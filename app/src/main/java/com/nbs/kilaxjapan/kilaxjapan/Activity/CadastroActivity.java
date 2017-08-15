d3cbf11eb084d41be1fe685498945cb0572c940c
Guilherme Camargo <guilhermecamargo57@hotmail.com>
2017-08-15 02:15:30 -0300

    Activity e Layout teste para Trabalho de APOO (Análise e Projeto Orientado a Objetos)

---
+++ app/src/main/java/com/nbs/kilaxjapan/kilaxjapan/Activity/CadastroActivity.java
@@ -0,0 +1,15 @@
+package com.nbs.kilaxjapan.kilaxjapan.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.nbs.kilaxjapan.kilaxjapan.R;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+}
