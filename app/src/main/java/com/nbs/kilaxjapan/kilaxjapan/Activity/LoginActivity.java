63b414d61958b2af5607c87ebeb02fe46ce31b89
Guilherme Camargo <guilhermecamargo57@hotmail.com>
2017-08-15 02:16:31 -0300

    Activity teste para Trabalho de APOO (Análise e Projeto Orientado a Objetos)

---
+++ app/src/main/java/com/nbs/kilaxjapan/kilaxjapan/Activity/LoginActivity.java
@@ -0,0 +1,39 @@
+package com.nbs.kilaxjapan.kilaxjapan.Activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.nbs.kilaxjapan.kilaxjapan.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button login = (Button) findViewById(R.id.btnLogin);
+        Button cadastrar = (Button) findViewById(R.id.btnCadastrar);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+        cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, CadastroActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }
+
+}
